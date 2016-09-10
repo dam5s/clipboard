902c3249105d859103abb964590e002d288a13ba
Damien Le Berrigaud <damien@itsdamo.com>
2016-09-10 10:26:04 -0600

    Initial Commit

---
+++ Clipboard/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Clipboard
+//
+//  Created by Damien Le Berrigaud on 9/10/16.
+//  Copyright © 2016 Damien Le Berrigaud. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
